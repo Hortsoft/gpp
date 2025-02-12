b00b43971b5088099ab101ac33bf40be41b4d748
Hortsoft <Info@hortsoft.co.nz>
2025-02-12 16:35:43 +1300

    Header

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -28,10 +28,10 @@ function Header() {
   }
 
   return (
-    <header className="border-b border-gray-200">
+    <header className="border-b border-gray-200 bg-orange-600">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
         <Link href="/" className="flex items-center">
-          <Flower className="h-6 w-6 text-green-500" />
+          <Flower className="h-6 w-6 text-lime-400" />
           <span className="ml-2 text-xl font-semibold text-gray-900">Garden Projects</span>
         </Link>
         <div className="flex items-center space-x-4">
